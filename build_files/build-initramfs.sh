4649650fbcf67b83443f90c4bfff78fecf0fb624
Nathan <ndowens@artixlinux.org>
2025-07-04 21:44:03 -0500

    Update build-initramfs.sh

--- build_files/build-initramfs.sh
+++ build_files/build-initramfs.sh
@@ -11,6 +11,7 @@ log "Building initramfs"
 
 # Get kernel version and build initramfs
 KERNEL_VERSION="$(dnf5 repoquery --installed --queryformat='%{evr}.%{arch}' kernel)"
+for i in $KERNEL_VERSION; do
 /usr/bin/dracut \
   --no-hostonly \
   --kver "$KERNEL_VERSION" \
@@ -21,5 +22,5 @@ KERNEL_VERSION="$(dnf5 repoquery --installed --queryformat='%{evr}.%{arch}' kern
   -f "/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
 
 chmod 0600 "/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
-
+done
 log "Build completed"
