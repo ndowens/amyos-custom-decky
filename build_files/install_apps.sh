f758f2d1637b5fe5702076c185d9d950f4b7781f
Nathan <ndowens@artixlinux.org>
2025-07-05 22:25:12 -0500

    Update install_apps.sh

--- build_files/install_apps.sh
+++ build_files/install_apps.sh
@@ -61,10 +61,6 @@ declare -A RPM_PACKAGES=(
 	  akmods"
 )
 
-dnf5 -y remove akmods kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra kernel-devel-matched
-dnf5 -y install akmods kernel-cachyos-lto kernel-cachyos-lto-modules kernel-cachyos-lto-core \
-	kernel-cachyos-lto-devel-matched
-
 log "Starting Amy OS build process"
 log "Installing RPM packages"
 mkdir -p /var/opt
